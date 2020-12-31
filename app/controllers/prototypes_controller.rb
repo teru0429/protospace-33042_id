ab352dedaa530f44b9c464eb81941c53604470ca
teru0429 <75606622+teru0429@users.noreply.github.com>
2020-12-31 21:25:06 +0900

    “修正2”

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:edit, :destroy, :index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :destroy,]
 
   def index
